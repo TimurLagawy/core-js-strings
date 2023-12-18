7a8bf9838db29a7b7a16259d78a7370f1bdfa789
Timur Lagawy <timurtagirovich@mail.ru>
2023-12-18 17:45:31 +0100

    feat: add 30 working functions

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -484,8 +484,19 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  let res = '';
+  for (let i = 0; i < str.length; i += 1) {
+    const letter = str[i];
+
+    if (letter === letter.toLowerCase()) {
+      res += letter.toUpperCase();
+    } else {
+      res += letter.toLowerCase();
+    }
+  }
+
+  return res;
 }
 
 /**
